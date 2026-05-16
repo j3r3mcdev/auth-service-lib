e7739ab34cd7fb280fde3a3ea835c4ab6959b2d5
J3r3mC <j.corbeilla@outlook.fr>
2026-05-16 22:57:44 +0200

    Clean imports & architecture

--- src/middleware/advanced/index.ts
+++ src/middleware/advanced/index.ts
@@ -6,3 +6,11 @@ export * from "./path/path-check";
 export * from "./asn/asn-check";
 export * from "./geoip/geoip-check";
 export * from "./user-agent-filtering/user-agent-filtering-check";
+
+// Export des detectors/providers
+export * from "./geoip/geoip-provider";
+export * from "./sqli/sqli-detector";
+export * from "./xxs/xss-detector";
+export * from "./lfi/lfi-detector";
+export * from "./rfi/rfi-detector";
+export * from "./path/path-detector";
--- src/waf/index.ts
+++ src/waf/index.ts
@@ -1,25 +1,31 @@
-import { pathNormalization } from "../middleware/pre-auth/path-normalization";
-import { methodEnforcement } from "../middleware/pre-auth/method-enforcement";
-import { contentTypeCheck } from "../middleware/pre-auth/content-type-check";
-import { headerValidation } from "../middleware/pre-auth/header-validation";
-import { ipCheck } from "../middleware/pre-auth/ip-check";
-import { rateLimit } from "../middleware/pre-auth/rate-limit";
-import { anomalyDetection } from "../middleware/pre-auth/anomaly_detection";
-import { basicUserAgentCheck } from "../middleware/pre-auth/user-agent-check";
+import {
+  pathNormalization,
+  methodEnforcement,
+  contentTypeCheck,
+  headerValidation,
+  basicUserAgentCheck,
+  ipCheck,
+  rateLimit,
+  anomalyDetection,
+} from "../middleware";
 
-import { geoIpCheck } from "../middleware/advanced/geoip/geoip-check";
-import { sqliCheck } from "../middleware/advanced/sqli/sqli-check";
-import { xssCheck } from "../middleware/advanced/xxs/xss-check";
-import { lfiCheck } from "../middleware/advanced/lfi/lfi-check";
-import { rfiCheck } from "../middleware/advanced/rfi/rfi-check";
-import { pathCheck } from "../middleware/advanced/path/path-check";
+import {
+  geoIpCheck,
+  sqliCheck,
+  xssCheck,
+  lfiCheck,
+  rfiCheck,
+  pathCheck,
+} from "../middleware/advanced";
 
-import { GeoIpProvider } from "../middleware/advanced/geoip/geoip-provider";
-import { SqlInjectionDetector } from "../middleware/advanced/sqli/sqli-detector";
-import { XssDetector } from "../middleware/advanced/xxs/xss-detector";
-import { LfiDetector } from "../middleware/advanced/lfi/lfi-detector";
-import { RfiDetector } from "../middleware/advanced/rfi/rfi-detector";
-import { PathTraversalDetector } from "../middleware/advanced/path/path-detector";
+import {
+  GeoIpProvider,
+  SqlInjectionDetector,
+  XssDetector,
+  LfiDetector,
+  RfiDetector,
+  PathTraversalDetector,
+} from "../middleware/advanced";
 
 export interface WafConfig {
   geoIpProvider: GeoIpProvider;
